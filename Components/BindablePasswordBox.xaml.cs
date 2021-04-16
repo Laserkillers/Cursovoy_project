37a49874e4321c2e6880c4be3c9849f2b3153972
Laserkillers <spyon.alex@yandex.ru>
2021-04-16 23:40:58 +0300

    Add normal Password box
    Добавление поле пароля, чтобы из него передавалась информация

---
+++ Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cursovoy_project.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+
+        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox),
+                new PropertyMetadata(string.Empty));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = passwordBox.Password;
+        }
+    }
+}
