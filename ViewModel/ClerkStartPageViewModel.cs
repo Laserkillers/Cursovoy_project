f9c66671a8654656b474c8b75d8581ac69215d2f
Laserkillers <spyon.alex@yandex.ru>
2021-04-21 18:14:56 +0300

    add start Clerk page and do some functions
    Добавление страницы бухгалтера и реазизация некоторых функций

---
+++ ViewModel/ClerkStartPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Cursovoy_project.ViewModel
+{
+    class ClerkStartPageViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private IMainWindowsCodeBehind _MainCodeBehind;
+        public ClerkStartPageViewModel(IMainWindowsCodeBehind codeBehind)
+        {
+            if (codeBehind == null) throw new ArgumentNullException(nameof(codeBehind));
+            _MainCodeBehind = codeBehind;
+        }
+
+        /// <summary>
+        /// Реализация кнопки закрыть программу
+        /// </summary>
+        private RelayCommand _Close;
+        public RelayCommand Close
+        {
+            get { return _Close ??= new RelayCommand(OnGoToClosePage, CanGoClosePage); }
+        }
+        private void OnGoToClosePage()
+        {
+            _MainCodeBehind.ClosePage();
+        }
+        private bool CanGoClosePage()
+        {
+            return true;
+        }
+
+        private RelayCommand _GoToRecieveData;
+        public RelayCommand GoToRecieveData
+        {
+            get { return _GoToRecieveData ??= new RelayCommand(OnGoToRecieveData, CanGoToRecieveData); }
+        }
+        private void OnGoToRecieveData()
+        {
+            _MainCodeBehind.LoadClerksPage(Clerk_view_number.Receive_Data);
+        }
+        private bool CanGoToRecieveData()
+        {
+            return true;
+        }
+    }
+}
