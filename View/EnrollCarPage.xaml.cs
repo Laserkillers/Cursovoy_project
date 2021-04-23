2d237a3cc10152b5306d98a0f12251231bea57d5
Laserkillers <spyon.alex@yandex.ru>
2021-04-24 02:01:05 +0300

    add enroll car page add realise some methods
    Добавление страницы записи на приёи, а также создание некоторых методом для частичной реализации данной страницы

---
+++ View/EnrollCarPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cursovoy_project.View
+{
+    /// <summary>
+    /// Логика взаимодействия для EnrollCarPage.xaml
+    /// </summary>
+    public partial class EnrollCarPage : UserControl
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public EnrollCarPage(User _Customer)
+        {
+            InitializeComponent();
+            Customer = _Customer;
+        }
+        private User Customer;
+        
+        AutoServiceContext db;
+
+        
+        /*
+         * <!--ItemsSource="{Binding SelectedItem}" -->
+        private void CreateListSpisok()
+        {
+            TimeSpan time = new TimeSpan(8, 0, 0);
+            TimeSpan add_time = new TimeSpan(1, 0, 0);
+            List<TimeSpan> list = new List<TimeSpan>() {time};
+            for (int i = 0; i <= 9; i++)
+            {
+                time.Add(add_time);
+                list.Add(time);
+            }
+        }
+        */
+    }
+}
---
+++ ViewModel/EnrollCarPageViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Cursovoy_project.ViewModel
+{
+    class EnrollCarPageViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private User Customer;
+        private AutoService record = new AutoService();
+        AutoServiceContext db;
+        public EnrollCarPageViewModel(User _Customer) { Customer = _Customer; }
+
+        private IMainWindowsCodeBehind _MainCodeBehind;
+        public EnrollCarPageViewModel(IMainWindowsCodeBehind codeBehind, User _Customer)
+        {
+            if (codeBehind == null) throw new ArgumentNullException(nameof(codeBehind));
+            Customer = _Customer;
+            _MainCodeBehind = codeBehind;
+            
+        }
+        /// <summary>
+        /// Реализация передачи данных из DatePicker
+        /// </summary>
+        private DateTime? _ReceptionTime;
+        public DateTime? ReceptionTime
+        {
+            get { return _ReceptionTime; }
+            set 
+            { 
+                _ReceptionTime = value;
+                record.IssureTime = (DateTime)value;
+                ListOfTimes = ReturnList();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ListOfTimes)));
+            }
+        }
+
+        private List<TimeSpan> _ListOfTimes;
+        public List<TimeSpan> ListOfTimes
+        {
+            get { return _ListOfTimes; }
+            set
+            {
+                _ListOfTimes = value;
+            }
+        }
+        /// <summary>
+        /// Создаёт список данных на основе выбора даты пользователем
+        /// </summary>
+        /// <returns></returns>
+        private List<TimeSpan> ReturnList()
+        {
+            db = new AutoServiceContext();
+            db.AutoServices.Load();
+            /*int Check_free_hours = db.AutoServices.Local
+                .Where(p=>( ))*/
+            TimeSpan time = new TimeSpan(8, 0, 0);
+            DateTime Date_time = record.IssureTime;
+            Date_time = Date_time.AddHours(time.Hours);
+            List<TimeSpan> list = new List<TimeSpan>();
+            for (int i = 0; Date_time.Hour <= 17; i++)
+            {
+                int Check_free_hours = db.AutoServices.Local
+                .Where(p => (p.ReceptionTime == Date_time))
+                .Count();
+                if (Check_free_hours == 0)
+                    list.Add(time);
+                time += TimeSpan.FromHours(1);
+                Date_time = Date_time.AddHours(1);
+            }
+            db.Dispose();
+            return list;
+        }
+
+        
+    }
+}
