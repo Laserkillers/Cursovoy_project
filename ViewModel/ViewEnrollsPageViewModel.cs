2eefcac26ca10a689b5733a4ccee54edc7e9824b
Laserkillers <spyon.alex@yandex.ru>
2021-04-26 20:29:47 +0300

    add output data from db to screen

--- ViewModel/ViewEnrollsPageViewModel.cs
+++ ViewModel/ViewEnrollsPageViewModel.cs
@@ -12,6 +12,7 @@ namespace Cursovoy_project.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
         private User Customer;
+        AutoServiceContext db;
         IMainWindowsCodeBehind _MainCodeBehind;
         public ViewEnrollsPageViewModel(IMainWindowsCodeBehind codeBehind, User customer)
         {
@@ -19,6 +20,7 @@ namespace Cursovoy_project.ViewModel
 
             _MainCodeBehind = codeBehind;
             Customer = customer;
+            BuildTime(DateTime.Now);
         }
 
         private DateTime? _SelectDate;
@@ -28,9 +30,28 @@ namespace Cursovoy_project.ViewModel
             set
             {
                 _SelectDate = value;
-
+                BuildTime((DateTime)value);
                 PropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectDate)));
             }
+        }
+
+        private DateTime _StartDate;
+        private DateTime StartDate
+        {
+            get { return _StartDate; }
+            set { _StartDate = value; }
+        }
+        private DateTime _EndDate;
+        private DateTime EndDate
+        {
+            get { return _EndDate; }
+            set { _EndDate = value; }
+        }
+
+        private void BuildTime(DateTime dateTime)
+        {
+            StartDate = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0);
+            EndDate = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59);
         }
 
         private RelayCommand _GoToMasterMainPage;
@@ -56,12 +77,43 @@ namespace Cursovoy_project.ViewModel
 
         private void OnDoQuery()
         {
-
+            ListOfEnrolls = BuildList();
         }
         private bool CanDoQuery()
         {
             return true;
         }
+        private List<AutoService> _ListOfEnrolls;
+        public List<AutoService> ListOfEnrolls
+        {
+            get { return _ListOfEnrolls; }
+            set
+            {
+                _ListOfEnrolls = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ListOfEnrolls)));
+            }
+        }
 
+        private string _SumOfEnrolls;
+        public string SumOfEnrolls
+        {
+            get { return _SumOfEnrolls; }
+            set
+            {
+                _SumOfEnrolls = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(SumOfEnrolls)));
+            }
+        }
+
+        private List<AutoService> BuildList()
+        {
+            db = new AutoServiceContext();
+            db.AutoServices.Load();
+            List<AutoService> result = db.AutoServices.Local
+                .Where(p => ((p.ReceptionTime > StartDate) && (p.ReceptionTime < EndDate)))
+                .ToList();
+            SumOfEnrolls = result.Count.ToString() + " машин";
+            return result;
+        }
     }
 }
