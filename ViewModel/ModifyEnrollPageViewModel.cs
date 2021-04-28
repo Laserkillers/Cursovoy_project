cbb69393b124f0926d57b55c5d8beda4dd543e82
Laserkillers <spyon.alex@yandex.ru>
2021-04-28 23:47:08 +0300

    add many fields to get/receive/send data to db and from application

--- ViewModel/ModifyEnrollPageViewModel.cs
+++ ViewModel/ModifyEnrollPageViewModel.cs
@@ -8,14 +8,17 @@ using System.Text;
 
 namespace Cursovoy_project.ViewModel
 {
-    class ModifyEnrollPageViewModel:INotifyPropertyChanged
+    class ModifyEnrollPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
-
         private User Customer = new User();
+
         private List<AutoService> Result = new List<AutoService> { };
         private IMainWindowsCodeBehind _MainCodeBehind;
         private AutoServiceContext db;
+
+        private ClientsCarDatum car = new ClientsCarDatum();
+        private AutoService Record = new AutoService();
 
         public ModifyEnrollPageViewModel(IMainWindowsCodeBehind codeBehind, User customer)
         {
@@ -43,7 +46,6 @@ namespace Cursovoy_project.ViewModel
             StartDate = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0);
             EndDate = new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59);
         }
-        //Добавить кучу связей!!!
         /// <summary>
         /// Список машин, которые в ремонте
         /// </summary>
@@ -63,12 +65,13 @@ namespace Cursovoy_project.ViewModel
             db.AutoServices.Load();
             List<string> temporary = new List<string>();
             Result = db.AutoServices.Local
-                .Where(p=>((p.IssureTime == null)||(p.Cost == null)||(p.IssureTime > DateTime.Now)))
+                .Where(p => ((p.IssureTime == null) || (p.Cost == null) || (p.IssureTime > DateTime.Now)))
                 .ToList();
             foreach (AutoService c in Result)
             {
                 temporary.Add(c.GosNumber);
             }
+            db.Dispose();
             return temporary;
         }
 
@@ -83,6 +86,194 @@ namespace Cursovoy_project.ViewModel
         }
         private bool CanGoMenu()
         {
+            return true;
+        }
+
+        private string _SelectedCar;
+        public string SelectedCar
+        {
+            get { return _SelectedCar; }
+            set
+            {
+                _SelectedCar = value;
+                car.GosNumber = value;
+                Record.GosNumber = value;
+                UpdateBoxes();
+            }
+        }
+
+        private void UpdateBoxes()
+        {
+            db = new AutoServiceContext();
+            db.AutoServices.Load();
+            db.ClientsCarData.Load();
+            Record = db.AutoServices.Local
+                .Where(p => (p.GosNumber == Record.GosNumber)&&((p.IssureTime == null) || (p.Cost == null) || (p.IssureTime > DateTime.Now)))
+                .FirstOrDefault();
+            car = db.ClientsCarData.Local
+                .Where(p=> (p.GosNumber == car.GosNumber))
+                .FirstOrDefault();
+            GosNumber = Record.GosNumber;
+            CarBrend = car.CarBrend;
+            CarModel = car.CarModel;
+            CarFault = Record.Fault;
+            FaultsCost = Record.Cost.ToString();
+            ReceptionCarTime = Record.ReceptionTime.ToString();
+            db.Dispose();
+        }
+
+        private string _GosNumber;
+        public string GosNumber
+        {
+            get { return _GosNumber; }
+            set
+            {
+                _GosNumber = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(GosNumber)));
+            }
+        }
+        private string _CarBrend;
+        public string CarBrend
+        {
+            get { return _CarBrend; }
+            set
+            {
+                _CarBrend = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CarBrend)));
+            }
+        }
+        private string _CarModel;
+        public string CarModel
+        {
+            get { return _CarModel; }
+            set
+            {
+                _CarModel = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CarModel)));
+            }
+        }
+        private string _CarFault;
+        public string CarFault
+        {
+            get { return _CarFault; }
+            set
+            {
+                _CarFault = value;
+                Record.Fault = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(CarFault)));
+            }
+        }
+        private string _FaultsCost;
+        public string FaultsCost
+        {
+            get { return _FaultsCost; }
+            set
+            {
+                _FaultsCost = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(FaultsCost)));
+            }
+        }
+        private string _ReceptionCarTime;
+        public string ReceptionCarTime
+        {
+            get { return _ReceptionCarTime; }
+            set
+            {
+                _ReceptionCarTime = value;
+                Record.ReceptionTime = Convert.ToDateTime(value);
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ReceptionCarTime)));
+            }
+        }
+        private DateTime? _IssureDate;
+        public DateTime? IssureDate
+        {
+            get { return _IssureDate; }
+            set
+            {
+                _IssureDate = value;
+                Record.IssureTime = value;
+                ListOfTimes = ReturnList();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(IssureDate)));
+            }
+        }
+        private TimeSpan _SelectedTime;
+        public TimeSpan SelectedTime
+        { 
+            get { return _SelectedTime; }
+            set
+            {
+                _SelectedTime = value;
+                DateTime dateNoTime = (DateTime)_IssureDate;
+                dateNoTime = dateNoTime.AddHours(_SelectedTime.Hours);
+                Record.IssureTime = dateNoTime;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedTime)));
+            }
+        }
+
+        private List<TimeSpan> _ListOfTimes;
+        public List<TimeSpan> ListOfTimes
+        {
+            get { return _ListOfTimes; }
+            set
+            {
+                _ListOfTimes = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ListOfTimes)));
+            }
+        }
+        private List<TimeSpan> ReturnList()
+        {
+            db = new AutoServiceContext();
+            db.AutoServices.Load();
+            TimeSpan time = new TimeSpan(8, 0, 0);
+            DateTime Date_time = (DateTime)Record.IssureTime;
+            Date_time = Date_time.AddHours(time.Hours);
+            List<TimeSpan> list = new List<TimeSpan>();
+            for (int i = 0; Date_time.Hour <= 17; i++)
+            {
+                int Check_free_hours = db.AutoServices.Local
+                .Where(p => (p.IssureTime == Date_time))
+                .Count();
+                if (Check_free_hours == 0)
+                    list.Add(time);
+                time += TimeSpan.FromHours(1);
+                Date_time = Date_time.AddHours(1);
+            }
+            db.Dispose();
+            return list;
+        }
+
+        private RelayCommand _GoSave;
+        public RelayCommand GoSave
+        {
+            get { return _GoSave ??= new RelayCommand(OnGoSave, CanGoSave); }
+        }
+
+        private void OnGoSave()
+        {
+            try
+            {
+                SaveChangesToBd();
+                _MainCodeBehind.ShowMessageBox("Успешно сохранено в базе данных");
+            }
+            catch (Exception e)
+            {
+                _MainCodeBehind.ShowMessageBox(e.ToString());
+            }
+        }
+        private bool CanGoSave()
+        {
+            return true;
+        }
+
+        private bool SaveChangesToBd()
+        {
+            if (Record.Fault == null) throw new Exception("Не заполнено поле поломка авто");
+            Record.Cost = Convert.ToDecimal(FaultsCost);
+            db = new AutoServiceContext();
+            db.Update(Record);
+            db.SaveChanges();
+
+            db.Dispose();
             return true;
         }
     }
