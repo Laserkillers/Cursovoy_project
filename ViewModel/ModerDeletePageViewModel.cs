4a877a8a20aa77457bbdfce8bca099f4ebe24c28
Laserkillers <spyon.alex@yandex.ru>
2021-05-01 15:45:31 +0300

    Add ViewModel to ModerDeletePage

---
+++ ViewModel/ModerDeletePageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Data;
+
+namespace Cursovoy_project.ViewModel
+{
+    class ModerDeletePageViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate {};
+        private User Customer;
+        private IMainWindowsCodeBehind _MainCodeBehind;
+
+        public void ModeratorMainPageViewModel(IMainWindowsCodeBehind codeBehind, User customer)
+        {
+            if (codeBehind == null) throw new ArgumentNullException(nameof(codeBehind));
+            _MainCodeBehind = codeBehind;
+            Customer = customer;
+        }
+
+        private List<string> _ListOfRowToDelete;
+        public List<string> ListOfRowToDelete
+        {
+            get { return _ListOfRowToDelete; }
+            set
+            {
+                _ListOfRowToDelete = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ListOfRowToDelete)));
+            }
+        }
+        private string _SelectedRow;
+        public string SelectedRow
+        {
+            get { return _SelectedRow; }
+            set
+            {
+                _SelectedRow = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(SelectedRow)));
+            }
+        }
+    }
+}
